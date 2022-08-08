e986ff804a1a5607cadd7f494116ab0663a33643
soohyun <suhyeon.kan@gmail.com>
2022-08-08 21:28:34 +0900

    feat: 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import './scss/style.scss';
+import { Route, Switch } from 'react-router-dom';
+
+//common
 import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+//main
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+//sub
+import Department from './components/sub/Department';
+import Community from './components/sub/Community';
+import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+import './scss/style.scss';
 
 function App() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Switch>
+				{/* 메인페이지 전용 라우터  */}
+				<Route exact path='/'>
+					<Header type={'main'} />
+					<Visual />
+					<Content />
+				</Route>
+
+				{/* 서브페이지 라우터 */}
+				<Route path='/'>
+					<Header type={'sub'} />
+				</Route>
+			</Switch>
+
+			<Route path='/department' component={Department} />
+			<Route path='/community' component={Community} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/location' component={Location} />
+			<Route path='/members' component={Members} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
