e3c68c04d60f13d6197fb4ae27962d2f3d7d4bc2
soohyun <suhyeon.kan@gmail.com>
2022-08-10 09:08:09 +0900

    feat: department 페이지 구조 잡고 이미지 넣음

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,8 +1,39 @@
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function Department() {
+	const path = process.env.PUBLIC_URL;
+	const [Members, setMembers] = useState([]);
+
+	useEffect(() => {
+		axios.get(path + '/DB/members.json').then((json) => {
+			setMembers(json.data.members);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(Members);
+	}, [Members]);
+
 	return (
 		<Layout name={'Department'}>
-			<p>Department</p>
+			{Members.map((member, idx) => (
+				<article key={idx}>
+					<div className='inner'>
+						<div className='picFrame'>
+							<div className='reflect'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</div>
+					</div>
+				</article>
+			))}
 		</Layout>
 	);
 }
