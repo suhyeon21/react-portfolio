40868cb76358bebcbd8b68e762fb0c867c5263e4
soohyun <suhyeon.kan@gmail.com>
2022-08-11 08:14:43 +0900

    feat: youtube 데이터 호출하고 출력

---
+++ src/components/common/Popup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+function Popup(props) {
+	useEffect(() => {
+		document.body.style.overflowY = 'hidden';
+
+		return () => {
+			document.body.style.overflowY = 'auto';
+		};
+	});
+
+	return (
+		<aside className='popup'>
+			<div className='con'></div>
+			<span className='close' onClick={() => props.setOpen(false)}>
+				close
+			</span>
+		</aside>
+	);
+}
+
+export default Popup;
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,9 +1,38 @@
 import Layout from '../common/Layout';
+import Popup from '../common/Popup';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-export default function Youtube() {
+function Youtube() {
+	const [Vids, setVids] = useState([]);
+	const [Open, setOpen] = useState(false);
+
+	useEffect(() => {
+		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
+		const playlist = 'PLHtvRFLN5v-VD95TBpr5Dh2zguWCjjmMG';
+		const num = 6;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+		axios.get(url).then((json) => {
+			setVids(json.data.items);
+		});
+	});
+
 	return (
-		<Layout name={'Youtube'}>
-			<p>Youtube</p>
-		</Layout>
+		<>
+			<Layout name={'Youtube'}>
+				{Vids.map((vid, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{vid.snippet.title}</h2>
+							<p>{vid.snippet.description}</p>
+							<img src={vid.snippet.thumbnails.standard.url} alt={vid.title} />
+							<span>{vid.snippet.publishedAt}</span>
+						</article>
+					);
+				})}
+			</Layout>
+			{Open && <Popup setOpen={setOpen}></Popup>}
+		</>
 	);
 }
+export default Youtube;
