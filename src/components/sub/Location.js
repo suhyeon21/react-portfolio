4b6c18b96ae9c303a915d65eb5e98a671dd10739
soohyun <suhyeon.kan@gmail.com>
2022-08-11 14:57:06 +0900

    feat: kakao map 활용하여 location 페이지에 지도 출력

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,9 +1,21 @@
 import Layout from '../common/Layout';
+import { useEffect, useRef } from 'react';
 
 function Location() {
+	const { kakao } = window;
+	const container = useRef(null);
+	const option = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	};
+	// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+	useEffect(() => {
+		new kakao.maps.Map(container.current, option);
+	}, []);
+
 	return (
 		<Layout name={'Location'}>
-			<p>Location</p>
+			<div id='map' ref={container}></div>
 		</Layout>
 	);
 }
