838bf0c72c87cf6e01ccad4240842734e69dc98c
soohyun <suhyeon.kan@gmail.com>
2022-08-14 14:29:53 +0900

    feat: 현재 입력하고 있는 input의 정보값을 onChange 이벤트로 감지해서 state에 저장했다가 value 속성으로 출력

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,8 +1,40 @@
 import Layout from '../common/Layout';
+import { useState } from 'react';
 function Members() {
+	const initVal = {
+		userid: '',
+	};
+	const [Val, setVal] = useState(initVal);
+
 	return (
 		<Layout name={'Members'}>
-			<p>Members</p>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1' wid='600'>
+						<caption>회원가입 정보 입력</caption>
+						<tbody>
+							<tr>
+								<th>
+									<label htmlFor='userid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										placeholder='아이디를 입력하세요'
+										name='userid'
+										id='userid'
+										value={Val.userid}
+										onChange={(e) => {
+											setVal({ ...Val, userid: e.target.value });
+										}}
+									/>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
