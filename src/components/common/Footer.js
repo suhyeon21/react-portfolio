d1d1536577db18687559949781fb89045b36af5b
soohyun <suhyeon.kan@gmail.com>
2022-08-15 16:27:00 +0900

    feat: footer 컴포넌트 구조 완성

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faDribbble } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<div className='upper'>
+				<h1>logo</h1>
+
+				<nav>
+					<dl>
+						<dt>COMMUNITY</dt>
+						<dd>About</dd>
+						<dd>Submit on Issue</dd>
+						<dd>Github Repo</dd>
+						<dd>Slack</dd>
+					</dl>
+
+					<dl>
+						<dt>GETTING STARTED</dt>
+						<dd>Introduction</dd>
+						<dd>Documentation</dd>
+						<dd>Usage</dd>
+						<dd>Globals</dd>
+						<dd>Elements</dd>
+						<dd>Collections</dd>
+						<dd>Themes</dd>
+					</dl>
+
+					<dl>
+						<dt>RESOURCES</dt>
+						<dd>API</dd>
+						<dd>Form ValidationProduct</dd>
+						<dd>Visibility</dd>
+						<dd>Accessibility</dd>
+						<dd>Comnnunity</dd>
+						<dd>Design Defined</dd>
+						<dd>Markeplace</dd>
+					</dl>
+				</nav>
+			</div>
+
+			<div className='lower'>
+				<p>&copy; 2022 CONVERSE. All rights reserved.</p>
+
+				<ul className='terms'>
+					<li>Terms of Service</li>
+					<li>Privacy Policy</li>
+					<li>Security</li>
+					<li>Sitemap</li>
+				</ul>
+
+				<ul className='sns'>
+					<li>
+						<FontAwesomeIcon icon={faTwitter} />
+					</li>
+					<li>
+						<FontAwesomeIcon icon={faGithub} />
+					</li>
+					<li>
+						<FontAwesomeIcon icon={faFacebook} />
+					</li>
+					<li>
+						<FontAwesomeIcon icon={faDribbble} />
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
