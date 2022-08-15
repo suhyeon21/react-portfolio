b2acb47796b7c77fa84ac4309b8bc5813f189168
soohyun <suhyeon.kan@gmail.com>
2022-08-16 06:13:14 +0900

    feat: 문자 데이터 가공 처리

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,15 +1,14 @@
 import Layout from '../common/Layout';
-import Popup from '../common/Popup';
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function Youtube() {
 	const [Vids, setVids] = useState([]);
-	const [Open, setOpen] = useState(false);
 
 	useEffect(() => {
-		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
-		const playlist = 'PLHtvRFLN5v-VD95TBpr5Dh2zguWCjjmMG';
+		const key = 'AIzaSyB9zeQFs7g9K4g_p4oCeqToiPG81AmLFhk';
+		const playlist = 'PLUTOYlZjKt_ZJJO4HwADJIalRw28uqXVe';
 		const num = 6;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
 		axios.get(url).then((json) => {
@@ -18,21 +17,25 @@ function Youtube() {
 	});
 
 	return (
-		<>
-			<Layout name={'Youtube'}>
-				{Vids.map((vid, idx) => {
-					return (
-						<article key={idx}>
-							<h2>{vid.snippet.title}</h2>
-							<p>{vid.snippet.description}</p>
+		<Layout name={'Youtube'}>
+			{Vids.map((vid, idx) => {
+				return (
+					<article key={idx}>
+						<h2>
+							{vid.snippet.title.length > 50
+								? vid.snippet.title.substr(0, 30) + '...'
+								: vid.snippet.title}
+						</h2>
+						<div className='txt'>
+							<span>{vid.snippet.publishedAt.split('T')[0]}</span>
+						</div>
+						<div className='pic'>
 							<img src={vid.snippet.thumbnails.standard.url} alt={vid.title} />
-							<span>{vid.snippet.publishedAt}</span>
-						</article>
-					);
-				})}
-			</Layout>
-			{Open && <Popup setOpen={setOpen}></Popup>}
-		</>
+						</div>
+					</article>
+				);
+			})}
+		</Layout>
 	);
 }
 export default Youtube;
