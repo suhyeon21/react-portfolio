1bb11456fabf39e38b8f28f3a9feb388638e7b7b
soohyun <suhyeon.kan@gmail.com>
2022-08-15 15:16:10 +0900

    feat: select 메뉴 인증 처리 구현

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -9,6 +9,7 @@ function Members() {
 		pwd2: '',
 		gender: null,
 		interests: null,
+		edu: '',
 	};
 	const [Val, setVal] = useState(initVal);
 	//인증 실패시 출력될 에러메시지 담을 state
@@ -55,6 +56,10 @@ function Members() {
 		if (!value.interests) {
 			errs.interests = '관심사를 하나 이상 선택하세요';
 		}
+		//edu 인증처리
+		if (value.edu === '') {
+			errs.edu = '최종학력을 선택하세요';
+		}
 		return errs;
 	};
 
@@ -89,6 +94,12 @@ function Members() {
 			if (el.checked) isCheck = true;
 		});
 		setVal({ ...Val, [name]: isCheck });
+	};
+
+	//select 전용 함수
+	const handleSelect = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
 	};
 
 	useEffect(() => {
@@ -219,6 +230,23 @@ function Members() {
 									<span className='err'>{Err.interests}</span>
 								</td>
 							</tr>
+							{/* edu */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='edu'>EDUCATION</label>
+								</th>
+								<td>
+									<select name='edu' id='edu'>
+										<option value=''>최종 학력을 선택하세요</option>
+										<option value='elementary-school'>초등학교 졸업</option>
+										<option value='middle-school'>중학교 졸업</option>
+										<option value='high-school'>고등학교 졸업</option>
+										<option value='college'>대학교 졸업</option>
+									</select>
+									<span className='err'>{Err.edu}</span>
+								</td>
+							</tr>
+
 							{/* btnset */}
 							<tr>
 								<th colspan='2'>
