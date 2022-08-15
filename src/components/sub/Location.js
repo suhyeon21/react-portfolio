8122b6e4565f27cbdec127ef72f5e8b548fd1c61
soohyun <suhyeon.kan@gmail.com>
2022-08-15 17:24:39 +0900

    feat: 지도 마커 생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -8,9 +8,14 @@ function Location() {
 		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
 		level: 3, // 지도의 확대 레벨
 	};
+
+	const marker = new kakao.maps.Marker({
+		position: option.center,
+	});
 	// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 	useEffect(() => {
-		new kakao.maps.Map(container.current, option);
+		const map_instance = new kakao.maps.Map(container.current, option);
+		marker.setMap(map_instance);
 	}, []);
 
 	return (
