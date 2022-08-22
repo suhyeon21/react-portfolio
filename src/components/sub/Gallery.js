b310ac3cab863c59301ca486d4b61bd1a7dcb442
soohyun <suhyeon.kan@gmail.com>
2022-08-23 08:25:07 +0900

    Feat: async await로 코드 동기화 시키고 이후 frame요소 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,30 +1,35 @@
 import Layout from '../common/Layout';
 import Popup from '../common/Popup';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function Gallery() {
-	const key = '5f93204b89f778b6700e782d390ca6ea';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const id = '196184841@N06';
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
-
+	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Index, setIndex] = useState(0);
 	const [Open, setOpen] = useState(false);
 
-	useEffect(() => {
-		axios.get(url).then((json) => {
+	const getFlickr = async () => {
+		const key = '5f93204b89f778b6700e782d390ca6ea';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 20;
+		const id = '196184841@N06';
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+
+		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
-	}, []);
+
+		frame.current.classList.add('on');
+	};
+
+	useEffect(getFlickr, []);
 
 	return (
 		<>
 			<Layout name={'Gallery'}>
-				<div className='frame on'>
+				<div className='frame' ref={frame}>
 					{Items.map((pic, idx) => {
 						return (
 							<article
