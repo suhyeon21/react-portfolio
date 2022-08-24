bd7b55a033c1dbc749358a18483fd0c4f0f52870
soohyun <suhyeon.kan@gmail.com>
2022-08-25 08:56:00 +0900

    Feat: sub title 이미지 추가

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -10,6 +10,7 @@ function Layout({ children, name }) {
 	return (
 		<section className={`content ${name}`} ref={frame}>
 			<figure>
+				<img src={`${process.env.PUBLIC_URL}/img/${name}.jpg`} alt={name} />
 				<h1>{name}</h1>
 			</figure>
 			<div className='inner'>{children}</div>
