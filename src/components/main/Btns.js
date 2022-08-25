fc21d944109a84847d57c0c2672a44237b75a323
soohyun <suhyeon.kan@gmail.com>
2022-08-26 08:24:53 +0900

    Feat: 버튼 활성화지점 커스터마이징

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -15,14 +15,19 @@ function Btns() {
 	};
 
 	//스크롤시 버튼 활성화 함수
+
 	const activation = () => {
+		const base = -window.innerHeight / 2;
 		const scroll = window.scrollY;
 		const btns = btnRef.current.children;
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
 
 		pos.current.map((pos, idx) => {
-			if (scroll >= pos) {
+			if (scroll >= pos + base) {
 				for (const btn of btns) btn.classList.remove('on');
 				btns[idx].classList.add('on');
+				for (const sec of secs) sec.classList.remove('on');
+				secs[idx].classList.add('on');
 			}
 		});
 	};
@@ -30,6 +35,9 @@ function Btns() {
 	//윈도우객체에 리사이즈, 스크롤 이벤트 연결
 	useEffect(() => {
 		btnRef.current.children[0].classList.add('on');
+		btnRef.current.parentElement
+			.querySelectorAll('.myScroll')[0]
+			.classList.add('on');
 		getPos();
 		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', activation);
