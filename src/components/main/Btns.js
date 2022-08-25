b51f7750bc8156e467dff617f19abbfe96bc18a5
soohyun <suhyeon.kan@gmail.com>
2022-08-26 07:59:45 +0900

    Feat: 스크롤 모션 코드 개선

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,29 +1,24 @@
 import Anime from '../../assets/Anime';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function Btns() {
 	const pos = useRef([]);
 	const btnRef = useRef(null);
-	const [Index, setIndex] = useState(0);
-	const [Scrolled, setScrolled] = useState(0);
+	const speed = 500;
 
-	//섹션의 세로 위치값을 구하는 함수
+	//세로 스크롤 위치값 구하는 함수
 	const getPos = () => {
 		pos.current = [];
-		const secs = btnRef.current.querySelectorAll('.myScroll');
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
 		for (const sec of secs) pos.current.push(sec.offsetTop);
-		console.log(pos.current);
 	};
 
-	//스크롤 위치에 따라서 버튼 활성화 함수
+	//스크롤시 버튼 활성화 함수
 	const activation = () => {
 		const scroll = window.scrollY;
 		const btns = btnRef.current.children;
-		setScrolled(scroll);
-		//pos.current에 등록된 각 섹션의 세로 위치값을 반복
+
 		pos.current.map((pos, idx) => {
-			//현재 스크롤된 거리값이 각 섹션의 위치값보다 같거나 크면
-			//기존 버튼 모두 비활성화 시키고 해당 순번의 버튼만 활성화
 			if (scroll >= pos) {
 				for (const btn of btns) btn.classList.remove('on');
 				btns[idx].classList.add('on');
@@ -31,86 +26,34 @@ function Btns() {
 		});
 	};
 
+	//윈도우객체에 리사이즈, 스크롤 이벤트 연결
 	useEffect(() => {
 		getPos();
 		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', activation);
-
 		return () => {
-			//window 객체의 경우 remove 이벤트 같이 걸어줘야함
 			window.removeEventListener('resize', getPos);
 			window.removeEventListener('scroll', activation);
 		};
 	}, []);
 
-	useEffect(() => {
-		console.log(Index);
-		new Anime(window, {
-			prop: 'scroll',
-			value: pos.current[Index],
-			duration: 500,
-		});
-	}, [Index]);
-
 	return (
 		<ul className='scroll_navi' ref={btnRef}>
-			<li
-				className='on'
-				onClick={() => {
-					if (Index === 0) {
-						if (Scrolled !== pos[0]) {
-							new Anime(window, {
-								prop: 'scroll',
-								value: pos.current[0],
-								duration: 500,
-							});
-						}
-					} else {
-						setIndex(0);
-					}
-				}}></li>
-			<li
-				onClick={() => {
-					if (Index === 0) {
-						if (Scrolled !== pos[1]) {
-							new Anime(window, {
-								prop: 'scroll',
-								value: pos.current[1],
-								duration: 500,
-							});
-						}
-					} else {
-						setIndex(1);
-					}
-				}}></li>
-			<li
-				onClick={() => {
-					if (Index === 0) {
-						if (Scrolled !== pos[2]) {
-							new Anime(window, {
-								prop: 'scroll',
-								value: pos.current[2],
-								duration: 500,
-							});
-						}
-					} else {
-						setIndex(2);
-					}
-				}}></li>
-			<li
-				onClick={() => {
-					if (Index === 0) {
-						if (Scrolled !== pos[3]) {
-							new Anime(window, {
-								prop: 'scroll',
-								value: pos.current[3],
-								duration: 500,
-							});
-						}
-					} else {
-						setIndex(3);
-					}
-				}}></li>
+			{Array(4)
+				.fill()
+				.map((_, idx) => {
+					return (
+						<li
+							key={idx}
+							onClick={() => {
+								new Anime(window, {
+									prop: 'scroll',
+									value: pos.current[idx],
+									duration: speed,
+								});
+							}}></li>
+					);
+				})}
 		</ul>
 	);
 }
