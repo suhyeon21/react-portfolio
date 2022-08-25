79ee1d35c907adbb9983f61aacafa5ffd2d6d225
soohyun <suhyeon.kan@gmail.com>
2022-08-25 17:25:10 +0900

    Feat: 메인 컴포넌트에서 데이터를 전달받기

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,13 +1,35 @@
 import { useState, useEffect } from 'react';
 
 function News() {
-	const [Posts, setPosts] = useState([]);
+	const getLocalData = () => {
+		const dummyPosts = [
+			{ title: 'Hello5', content: 'Here comes description in detail.' },
+			{ title: 'Hello4', content: 'Here comes description in detail.' },
+			{ title: 'Hello3', content: 'Here comes description in detail.' },
+			{ title: 'Hello2', content: 'Here comes description in detail.' },
+			{ title: 'Hello1', content: 'Here comes description in detail.' },
+		];
+
+		const data = localStorage.getItem('post');
+		console.log(data);
+
+		if (data) {
+			return JSON.parse(data);
+		} else {
+			console.log('더미 데이터 담기');
+			return dummyPosts;
+		}
+	};
+
+	const [Posts] = useState(getLocalData());
 
 	useEffect(() => {
-		let data = localStorage.getItem('post');
-		data = JSON.parse(data);
-		setPosts(data);
+		localStorage.setItem('post', JSON.stringify(Posts));
 	}, []);
+
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
 
 	return (
 		<section id='news'>
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -9,19 +9,8 @@ function Community() {
 
 	//로컬스토리지에 있는 데이터를 가져와서 다시 JSON객체로 parsing해서 리턴하는 함수
 	const getLocalData = () => {
-		const dummyPosts = [
-			{ title: 'Hello5', content: 'Here comes description in detail.' },
-			{ title: 'Hello4', content: 'Here comes description in detail.' },
-			{ title: 'Hello3', content: 'Here comes description in detail.' },
-			{ title: 'Hello2', content: 'Here comes description in detail.' },
-			{ title: 'Hello1', content: 'Here comes description in detail.' },
-		];
 		const data = localStorage.getItem('post');
-		if (data) {
-			return JSON.parse(data);
-		} else {
-			return dummyPosts;
-		}
+		return JSON.parse(data);
 	};
 
 	//초기 Posts스테이트에 로컬스토리지의 데이터를 가져와서 저장
